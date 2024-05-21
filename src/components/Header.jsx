cf7bd900c9c36358a281d4b065c9e51e37ef386b
Timothy Miamba <timal0361@gmail.com>
2024-05-21 19:01:32 +0200

    refactor: Add responsive styles for the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,19 @@
 /* eslint-disable react/prop-types */
 const Header = ({ scores }) => {
-  const { currentScore } = scores;
-  const { highScore } = scores;
+  const { currentScore, highScore } = scores;
 
   return (
-    <header>
-      <h1>Movie Memory Card Game</h1>
-      <p>Current Score: {currentScore}</p>
-      <p>High Score: {highScore}</p>
-      <p>
+    <header className={`grid md:grid-cols-2 md:grid-rows-3 gap-4 light`}>
+      <h1 className="text-4xl font-semibold md:col-span-2 flex justify-center items-center">
+        Timmy&apos;s Movie Memory Cards
+      </h1>
+      <p className="border text-xl font-semibold dark:text-slate-950 dark:bg-white rounded-xl shadow-lg flex justify-center items-center p-4 lg:ml-8">
+        Current Score: {currentScore} {currentScore === 1 ? "point" : "points"}
+      </p>
+      <p className="border text-xl font-semibold dark:text-slate-950 dark:bg-white rounded-xl shadow-lg flex justify-center items-center p-4 lg:mr-8">
+        High Score: {highScore} {highScore === 1 ? "point" : "points"}
+      </p>
+      <p className="md:col-span-2 flex justify-center items-center">
         Click on the images to get points. Your score will be reset if you click
         on an image more than once.
       </p>
