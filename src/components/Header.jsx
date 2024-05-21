db2c6ecf4f0b0c62dd2dda1737b5d3f2ba55fc74
Timothy Miamba <timal0361@gmail.com>
2024-05-21 15:50:36 +0200

    chore: Add game rules to the display

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,6 +8,10 @@ const Header = ({ scores }) => {
       <h1>Movie Memory Card Game</h1>
       <p>Current Score: {currentScore}</p>
       <p>High Score: {highScore}</p>
+      <p>
+        Click on the images to get points. Your score will be reset if you click
+        on an image more than once.
+      </p>
     </header>
   );
 };
