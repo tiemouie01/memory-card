e994ec4c068c379d1d3fd8e65a286e985c655c86
Timothy Miamba <timal0361@gmail.com>
2024-05-21 15:01:53 +0200

    refactor: Update Card and Game components to handle movie selection and scoring

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,12 @@
 /* eslint-disable react/prop-types */
-const Card = ({ title, posterPath, score, updateScores, randomizeMovies }) => {
+const Card = ({ title, posterPath, clickMovie }) => {
   const path = `https://image.tmdb.org/t/p/w185/${posterPath}`;
 
   return (
     <button
       className="card"
       onClick={() => {
-        randomizeMovies();
-        updateScores(score + 1);
+        clickMovie(title);
       }}
     >
       <img src={path} alt={`${title} poster`} />
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,17 +1,34 @@
 /* eslint-disable react/prop-types */
 import Card from "./Card";
+import { useState } from "react";
 
 const Game = ({ scores, updateScores, movies, randomizeMovies }) => {
+  const [selectedMovies, setSelectedMovies] = useState([]);
   const currentScore = scores.currentScore;
+
+  const clickMovie = (movieTitle) => {
+    // Randomize the order of the movies.
+    randomizeMovies();
+
+    if (selectedMovies.includes(movieTitle)) {
+      //If movie has already been selected, reset the score and empty the selected movie list.
+      updateScores(0);
+      setSelectedMovies([]);
+    } else {
+      //If movie has not been selected, increment the current score and add the movie to the selected list.
+      const selectedMoviesCopy = [...selectedMovies];
+      selectedMoviesCopy.push(movieTitle);
+      updateScores(currentScore + 1);
+      setSelectedMovies(selectedMoviesCopy);
+    }
+  };
 
   const movieList = movies.map((movie) => (
     <Card
       key={movie.id}
       title={movie.original_title}
       posterPath={movie.poster_path}
-      score={currentScore}
-      updateScores={updateScores}
-      randomizeMovies={randomizeMovies}
+      clickMovie={clickMovie}
     />
   ));
 
